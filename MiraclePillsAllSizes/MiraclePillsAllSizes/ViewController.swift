ade716d1acc4a06808588b6e37f17f44408e184d
Nabendu <nabendubiswas@Nabendus-Mac-mini.local>
2016-10-13 17:00:57 +0530

    MiraclePillAllSizes vFinal

--- MiraclePillsAllSizes/MiraclePillsAllSizes/ViewController.swift
+++ MiraclePillsAllSizes/MiraclePillsAllSizes/ViewController.swift
@@ -19,6 +19,26 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var pinLabel: UILabel!
     @IBOutlet weak var pinText: UITextField!
     
+    @IBOutlet weak var stateLabel: UILabel!
+    
+    @IBOutlet weak var cityText: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var addressText: UITextField!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var rupeeLabel: UILabel!
+    @IBOutlet weak var pillLabel: UILabel!
+    
+    @IBOutlet weak var pillImage: UIImageView!
+    
+    @IBOutlet weak var successImage: UIImageView!
+    
+    @IBOutlet weak var buynowImage: UIButton!
+    
     let states = ["Goa", "Delhi", "Mizoram", "Kerela", "Bihar", "Uttrakhand"]
     
     override func viewDidLoad() {
@@ -31,6 +51,29 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func buynowBtnPressed(_ sender: AnyObject) {
+        successImage.isHidden = false
+        buynowImage.isHidden = true
+        countryLabel.isHidden = true
+        countryText.isHidden = true
+        pinLabel.isHidden = true
+        pinText.isHidden = true
+        stateLabel.isHidden = true
+        cityText.isHidden = true
+        cityLabel.isHidden = true
+        addressText.isHidden = true
+        addressLabel.isHidden = true
+        nameText.isHidden = true
+        nameLabel.isHidden = true
+        rupeeLabel.isHidden = true
+        pillLabel.isHidden = true
+        pillImage.isHidden = true
+        statePickerBtn.isHidden = true
+        
+        
     }
 
     @IBAction func stateBtnPressed(_ sender: AnyObject) {
@@ -56,6 +99,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         statePickerBtn.setTitle(states[row], for: UIControlState.normal)
         statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryText.isHidden = false
+        pinLabel.isHidden = false
+        pinText.isHidden = false
     }
 
 }
