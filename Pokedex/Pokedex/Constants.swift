f8cc52965578fb110f99ca234f3f3672489d2bca
Nabendu <nabendubiswas@Nabendus-Mac-mini.local>
2016-11-13 22:27:53 +0530

    PokedexV3.0

---
+++ Pokedex/Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Nabendu Biswas on 13/11/16.
+//  Copyright © 2016 Nabendu Biswas. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
