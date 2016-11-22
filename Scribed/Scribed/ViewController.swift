e9a6beb8cfd550b24b5bd296996ca6a9f18642e1
Nabendu <nabendubiswas@Nabendus-Mac-mini.local>
2016-11-22 22:12:16 +0530

    ScribbedvFinal

--- Scribed/Scribed/ViewController.swift
+++ Scribed/Scribed/ViewController.swift
@@ -11,14 +11,60 @@ import Speech
 import AVFoundation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    @IBOutlet weak var transriptionTextField: UITextView!
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    
+    var audioPlayer: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
+        activitySpinner.isHidden = true
 
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
+    }
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error")
+                    }
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("There was an error \(error)")
+                        } else {
+                            self.transriptionTextField.text = result?.bestTranscription.formattedString
+                        }
+                        
+                        
+                    }
+                }
+            }
+            
+        }
+        
     }
 
 
+    @IBAction func playBtnPessed(_ sender: Any) {
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+    }
 
 }
 
